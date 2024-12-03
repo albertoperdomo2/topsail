629dc0c6fa6c63713a21d93f7e2b4c548959c447
Alberto Perdomo <aperdomo@redhat.com>
2024-12-03 14:34:31 +0000

    [ray]: add torch benchmark

--- projects/fine_tuning/toolbox/fine_tuning_ray_fine_tuning_job/workloads/ray-benchmark/entrypoint/entrypoint.sh
+++ projects/fine_tuning/toolbox/fine_tuning_ray_fine_tuning_job/workloads/ray-benchmark/entrypoint/entrypoint.sh
@@ -17,6 +17,14 @@ else
     echo "SCRIPT FAILED"
     # don't exit with a return code != 0, otherwise the RayJob->Job retries 3 times ...
 fi
+
+# need to figure our how to parse config hyperparams into this
+if python3 ./test_torch_benchmark.py run --num-runs 3 --num-epochs 20 --num-workers 4 --cpus-per-worker 8; then
+    echo "TORCH SCRIPT SUCCEEDED"
+else
+    echo "TORCH SCRIPT FAILED"
+fi
+
 set +x
 echo "*********"
 echo "*********"
