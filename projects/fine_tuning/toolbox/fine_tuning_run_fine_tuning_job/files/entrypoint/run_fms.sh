453c356b47005713afedbc9b69e5b4b6b2ab9859
Alberto Perdomo <aperdomo@redhat.com>
2025-01-02 11:19:18 +0000

    [fine_tuning]: launch using raw FSDP

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
@@ -16,22 +16,20 @@ if [[ $WORLD_SIZE == 1 ]]; then
     else
         echo "Running with a $NUM_GPUS GPUs"
     fi
-    python -m accelerate.commands.launch \
-     --num_processes=8 \
-     --num_machines=1 \
-     --mixed_precision=no \
-     --use_fsdp \
-     --fsdp_auto_wrap_policy="HYBRID_SHARD" \
-     --fsdp_forward_prefetch="BACKWARD_POST" \
-     --fsdp_offload_params="false" \
-     --fsdp_state_dict_type="SHARDED_STATE_DICT" \
-     --fsdp_sync_module_states="true" \
-     --fsdp_use_orig_params="false" \
-     --rdzv_backend="static" \
-     --same_network \
-     --machine_rank=0 \
-     --dynamo_backend=no \
+    python -m torch.distributed.run \
+     --nproc_per_node=8 \
+     --nnodes=1 \
+     --node_rank=0 \
+     --master_addr=localhost \
+     --master_port=29500 \
      --module \
-     tuning.sft_trainer
+     tuning.sft_trainer \
+     --fsdp_auto_wrap_policy="HYBRID_SHARD" \
+     --fsdp_backward_prefetch="BACKWARD_POST" \
+     --fsdp_forward_prefetch=True \
+     --fsdp_offload_params=False \
+     --fsdp_state_dict_type="SHARDED_STATE_DICT" \
+     --fsdp_sync_module_states=True \
+     --fsdp_use_orig_params=False
     exit 0
 fi
